41679164517acbbdb481bbb203c1acde807db4d4
BoltMaud <mathilde.boltenhage@outlook.com>
2024-10-09 19:53:24 +0200

    fix: boltco.fr as url

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,8 @@ import config from "./src/config/config.json";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://github.com/BoltMaud/frfr",
-  base: "./frfr/",
+  site: "https://boltco.fr/",
+  base: ".",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
   image: {
     service: squooshImageService(),
